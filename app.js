577c1114f9a24bbf3ed8fcee4d9cd7f6d0d322c3
Kyle Knight <knightkyle@outlook.com>
2023-04-20 22:09:52 -0700

    upload lab 7

--- app.js
+++ app.js
@@ -3,13 +3,51 @@
 console.log("I am in the HTML");
 alert("Welcome Ice Cream Lovers!");
 
-const usersName = prompt("What is your favorite Ice Cream Flavor?");
-console.log(usersName);
+function getName(){ 
+  const usersName = prompt("What is your favorite Ice Cream Flavor?");
+  return usersName;
 
-document.write("Yum! " + usersName + "!     Great Choice!" );
+}
+let icecreamFlavor = getName ()
 
-if (usersName == "strawberry"){
+
+
+function specialMessage(flavor){
+  // console.log (usersName)
+  if (flavor == ""){
+    icecreamFlavor = prompt ("Everyone has a favorite ice cream flavor?");
+
+
+    // document.write("Yum! " + usersName + "!   Great Choice");
+
+  }
+}
+specialMessage (icecreamFlavor)
+
+
+document.write("Yum! " + icecreamFlavor + "!     Great Choice!");
+
+if (icecreamFlavor == "strawberry"){
     alert("Number #1 Flavor");
 } else {
   alert("Yummy!");
 }
+
+
+
+
+
+
+
+
+
+function addTwoNumbers(){
+  let num1 = 4;
+  let num2 = 4;
+  let sum = num1 = num2;
+  console.log(sum);
+  return sum;
+
+
+}
+
